7a4c74625077649b1c749040cc85902de88161d1
Marlon Henrique Pereira Machado Saldanha <marlon.saldanha@MB-002976.local>
2022-05-20 15:25:58 -0300

    chore(database): configuring postgres on spring

--- grupo6/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO customers (customer_id) VALUES (2);
-INSERT INTO customers (customer_id) VALUES (3);
-INSERT INTO customers (customer_id) VALUES (4);
---
+++ grupo6/src/main/resources/db/data.sql
@@ -0,0 +1,7 @@
+CREATE TABLE customers (
+	customer_id integer
+);
+
+INSERT INTO customers (customer_id) VALUES (2);
+INSERT INTO customers (customer_id) VALUES (3);
+INSERT INTO customers (customer_id) VALUES (4);
